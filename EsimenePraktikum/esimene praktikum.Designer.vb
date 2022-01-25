ee29d58b33a0a84c8a589d474f04daa43b34647a
kivolo <kivolo@ICT-50128K>
2022-01-25 13:21:18 +0200

    Parandused 4.0

--- EsimenePraktikum/esimene praktikum.Designer.vb
+++ EsimenePraktikum/esimene praktikum.Designer.vb
@@ -22,11 +22,18 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.btnTeisenda = New System.Windows.Forms.Button()
         Me.txtKilomeetrid = New System.Windows.Forms.TextBox()
         Me.txtMillid = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.ilmKell = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SulgeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnTeisenda
@@ -71,18 +78,58 @@ Partial Class Form1
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Miles"
         '
+        'ilmKell
+        '
+        Me.ilmKell.AutoSize = True
+        Me.ilmKell.Location = New System.Drawing.Point(317, 315)
+        Me.ilmKell.Name = "ilmKell"
+        Me.ilmKell.Size = New System.Drawing.Size(130, 20)
+        Me.ilmKell.TabIndex = 5
+        Me.ilmKell.Text = "Siia ilmub kellaeg"
+        '
+        'Timer1
+        '
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 33)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SulgeToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(73, 29)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'SulgeToolStripMenuItem
+        '
+        Me.SulgeToolStripMenuItem.Name = "SulgeToolStripMenuItem"
+        Me.SulgeToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.SulgeToolStripMenuItem.Text = "Sulge"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ilmKell)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtMillid)
         Me.Controls.Add(Me.txtKilomeetrid)
         Me.Controls.Add(Me.btnTeisenda)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Esimene Praktikum"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -93,4 +140,9 @@ Partial Class Form1
     Friend WithEvents txtMillid As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents ilmKell As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SulgeToolStripMenuItem As ToolStripMenuItem
 End Class
--- EsimenePraktikum/esimene praktikum.vb
+++ EsimenePraktikum/esimene praktikum.vb
@@ -1,9 +1,7 @@
 ﻿Public Class Form1
 
     Private Const KM2MI = 1.609344
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
-    End Sub
 
     Private Sub btnTeisenda_Click(sender As Object, e As EventArgs) Handles btnTeisenda.Click
         Try
@@ -15,4 +13,24 @@
     Function teisenda(ByVal km As Double) As Double
         Return km / KM2MI
     End Function
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ilmKell.Text = DateTime.Now()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 2000
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        MsgBox("Leidsid vrjatud nupu!")
+    End Sub
+
+    Private Sub SulgeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SulgeToolStripMenuItem.Click
+        Me.Close()
+    End Sub
 End Class
