c76e79ee989ae949443c8378d22a383165ff5235
kivolo <kivolo@ICT-50128K>
2022-01-25 13:09:47 +0200

    Parandused 3.0

--- EsimenePraktikum/esimene praktikum.vb
+++ EsimenePraktikum/esimene praktikum.vb
@@ -7,9 +7,12 @@
 
     Private Sub btnTeisenda_Click(sender As Object, e As EventArgs) Handles btnTeisenda.Click
         Try
-            txtMillid.Text = txtKilomeetrid.Text / KM2MI
+            txtMillid.Text = teisenda(txtKilomeetrid.Text)
         Catch ex As Exception
             MsgBox("Viga sisendis!")
         End Try
     End Sub
+    Function teisenda(ByVal km As Double) As Double
+        Return km / KM2MI
+    End Function
 End Class
